a528d74904230e76027788d384b06f3879a353aa
Harsha <harsha.v.kota@gmail.com>
2024-06-04 10:02:59 +0530

    included delete blog functionality

--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,6 @@ def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-# @login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -46,4 +45,7 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
